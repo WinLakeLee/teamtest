f6739cf9b0c7471d4d5f5031490ee69b98bb7825
suhwan-99 <kaghop9@gamil.com>
2025-08-26 14:24:13 +0900

    오류 수정

--- src/main/java/com/example/teamtest/controller/UserController.java
+++ src/main/java/com/example/teamtest/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.teamtest.controller;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
