b9d40bd475c008d764dfee845bc54c3f4a1e07e0
panicbaek <woosikbaek@gmail.com>
2025-08-26 09:51:07 +0900

    signup/수정

--- src/main/java/com/example/teamtest/controller/UserController.java
+++ src/main/java/com/example/teamtest/controller/UserController.java
@@ -1,14 +1,26 @@
 package com.example.teamtest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.teamtest.domain.entity.UserEntity;
+import com.example.teamtest.service.UserService;
 
 @RestController
 public class UserController {
 
-	public ResponseEntity<?> singup () {
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/signup")
+	public ResponseEntity<?> singup (@RequestBody UserEntity user) {
+		userService.insert(user);
 		
-		return null;
+		return new ResponseEntity<>("회원가입 성공", HttpStatus.OK);
 	}
 	
 }
