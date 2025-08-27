852ba97b36663d2959a97b92551a9cf576ba1ded
WinLakeLee <lst2413@gmail.com>
2025-08-27 17:00:13 +0900

    랜덤 문제 생성 추가

--- src/main/java/com/example/teamtest/Repository/CategoryRepository.java
+++ src/main/java/com/example/teamtest/Repository/CategoryRepository.java
@@ -1,9 +1,16 @@
 package com.example.teamtest.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.teamtest.domain.entity.CategoryEntity;
+import com.example.teamtest.domain.Game;
+import com.example.teamtest.domain.QuestionType;
+
+
+
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer>{
-
+	Optional<CategoryEntity> findByGamenameAndDescription(Game gamename, QuestionType description);
 }
