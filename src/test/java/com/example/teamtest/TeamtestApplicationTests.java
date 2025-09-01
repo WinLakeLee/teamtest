2f9f2e52c99a6bb9eb5be208f46d17ba0458b0ae
WinLakeLee <lst2413@gmail.com>
2025-09-01 14:12:51 +0900

    Update TeamtestApplicationTests.java

--- src/test/java/com/example/teamtest/TeamtestApplicationTests.java
+++ src/test/java/com/example/teamtest/TeamtestApplicationTests.java
@@ -17,31 +17,6 @@ class TeamtestApplicationTests {
 	
 	@Test
 	void contextLoads() {
-		
-		List<GameList> lolScore = repository.findAllByOrderByLolScoreDesc();
-		lolScore.stream()
-	    .limit(2) // 2개만
-	    .forEach(System.out::println);
-		System.out.println("롤점수");
-		
-		List<GameList> bgScore = repository.findAllByOrderByBgScoreDesc();
-		bgScore.stream()
-	    .limit(2) 
-	    .forEach(System.out::println);
-		System.out.println("배그점수");
-
-		
-		List<GameList> scScore = repository.findAllByOrderByScScoreDesc();
-		scScore.stream()
-	    .limit(2) 
-	    .forEach(System.out::println);
-		System.out.println("스타점수");
-
-		List<GameList> msScore = repository.findAllByOrderByMsScoreDesc();
-		msScore.stream()
-	    .limit(2) 
-	    .forEach(System.out::println);
-		System.out.println("메이플점수");
 
 	}
 
